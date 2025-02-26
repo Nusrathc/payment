c22443e2e6458ff0ba2b0d739103063843e118a7
Nusrathc <nusrathcfaisal523@gmail.com>
2025-02-27 03:25:05 +0530

    Update environment.ts

--- micro-services/frontend/src/environments/environment.ts
+++ micro-services/frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://51.20.183.160:80/api',
-    authServiceUrl: 'http://51.20.183.160:80/api/auth',
-    paymentServiceUrl: 'http://51.20.183.160:80/api/payments'
+    apiUrl: 'http://16.16.159.217:80/api',
+    authServiceUrl: 'http://16.16.159.217:80/api/auth',
+    paymentServiceUrl: 'http://16.16.159.217:80/api/payments'
   };
